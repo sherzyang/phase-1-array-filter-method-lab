2cb5478925f7c7bb9b020611d06c3adf65afbe3d
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-26 11:33:39 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// // Code your solution here
+
+function findMatching(drivers, name){
+    let apple = item => item.toLowerCase() === name.toLowerCase();
+    let list = drivers.filter(apple);
+    return list
+}
+
+function fuzzyMatch(drivers,name) {
+    return drivers.filter(item => item[0] === name[0])
+}
+
+function matchName(drivers, oneDriver){
+    return drivers.filter(item => item.name === oneDriver);
+
+}
